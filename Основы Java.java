763e83513e035eea270ac540254bf6de12d14cc2
Hopes9 <62842164+Hopes9@users.noreply.github.com>
2021-11-08 13:03:03 +0300

    Update Основы Java.java

--- Основы Java.java
+++ Основы Java.java
@@ -1,7 +1,5 @@
-import java.util.OptionalInt;
-import java.util.Scanner;
-import java.util.Arrays;
-public class Rectangle {
+
+public class First {
 
     public static int[] concatArray(int[] a, int[] b) {
         if (a == null)
@@ -13,10 +11,22 @@ public class Rectangle {
         System.arraycopy(b, 0, r, a.length, b.length);
         return r;
     }
+
     public static int sum_int(int[] a) {
-        OptionalInt min = Arrays.stream(a).min();
-        OptionalInt max = Arrays.stream(a).max();
-        int raz = max.getAsInt() - min.getAsInt();
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        ;
+        int raz = max - min;
         return raz;
     }
 
@@ -29,8 +39,13 @@ public class Rectangle {
     }
 
     public static double sum_squares_massiv(int[] a) {
-        int sum_array = Arrays.stream(a).sum();
-        double r = Math.pow(sum_array, 2);
+
+        int max = 0;
+        for (int i = 0; i < a.length; i++) {
+            max += a[i];
+        }
+        ;
+        int r = max * max;
         return r;
     }
 
@@ -40,11 +55,12 @@ public class Rectangle {
         int start = 0;
         int fin = 0;
         a[0] = start;
-        a[a.length-1] = fin;
+        a[a.length - 1] = fin;
         a[0] = fin;
-        a[a.length-1] = start;
+        a[a.length - 1] = start;
         return a;
     }
+
     public static void main(String[] args) {
         int[] arrd = {1, 53, 413, 331, 756};
         int[] arr = {1, 5, 4, 3, 7};
@@ -53,18 +69,24 @@ public class Rectangle {
         System.out.print("Есть два массива: arrd = {");
         for (int i = 0; i < arrd.length; i++) {
             System.out.print(arrd[i]);
-            if (i != arrd.length - 1) {System.out.print(", ");}
+            if (i != arrd.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.print("} arr = {");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
-            if (i != arr.length - 1) {System.out.print(", ");}
+            if (i != arr.length - 1) {
+                System.out.print(", ");
+            }
         }
         int[] c = concatArray(arr, arrd);
         System.out.print("}\nСоединение массивов: {");
         for (int i = 0; i < c.length; i++) {
             System.out.print(c[i]);
-            if (i != c.length - 1) {System.out.print(", ");}
+            if (i != c.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.print("}\nРазницу между самым большим и самым маленьким числом в массиве - ");
 
@@ -73,19 +95,25 @@ public class Rectangle {
         System.out.print("\nДобавлять значения в массив старый массив arr = {");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
-            if (i != arr.length - 1) {System.out.print(", ");}
+            if (i != arr.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.print("} новый {");
         int[] b = addInArray(arr, 5);
         for (int i = 0; i < b.length; i++) {
             System.out.print(b[i]);
-            if (i != b.length - 1) {System.out.print(", ");}
+            if (i != b.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.print("}\nСмена первого и последнего элемента arrd = {");
         int[] a = revers_int(arrd);
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]);
-            if (i != a.length - 1) {System.out.print(", ");}
+            if (i != a.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.print("}");
     }
